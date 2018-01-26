1153f419181d832f55f0656228fc5574b942a37f
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2018-01-26 10:32:21 -1000

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -3,7 +3,8 @@ var Preload = function(game){};
 Preload.prototype = {
 
 	preload: function(){ 
-
+ this.game.load.image('tile', 'assets/tile.png');
+    this.game.load.spritesheet('dude', 'assets/Moonkeys.png', 32, 32);
 	},
 
 	create: function(){
