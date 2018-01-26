08f0d548204473ec5fd6fb018c23ecccd84f9bd7
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2018-01-26 10:35:11 -1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 var Main = function(game){
-
+var me = this;
+ 
+    //Get the dimensions of the tile we are using
+    me.tileWidth = me.game.cache.getImage('tile').width;
+    me.tileHeight = me.game.cache.getImage('tile').height;
+ 
+    //Set the background colour to blue
+    me.game.stage.backgroundColor = '479cde';
+ 
+    //Enable the Arcade physics system
+    me.game.physics.startSystem(Phaser.Physics.ARCADE);
+ 
+    //Add a platforms group to hold all of our tiles, and create a bunch of them
+    me.platforms = me.game.add.group();
+    me.platforms.enableBody = true;
+    me.platforms.createMultiple(250, 'tile');
+ 
 };
 
 Main.prototype = {
