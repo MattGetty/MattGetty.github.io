ffe06ab792092bf0b77c7dae1549e7c09bee8d8b
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2018-02-02 10:07:07 -1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,176 +1,149 @@
-var Main = function(game){
-var me = this;
- 
-   
-};
+var Jumper = function() {};
+Jumper.Play = function() {};
 
-Main.prototype = {
+Jumper.Play.prototype = {
 
-	create: function() {
-//Get the dimensions of the tile we are using
-    me.tileWidth = me.game.cache.getImage('tile').width;
-    me.tileHeight = me.game.cache.getImage('tile').height;
- 
-    //Set the background colour to blue
-    me.game.stage.backgroundColor = '479cde';
- 
-    //Enable the Arcade physics system
-    me.game.physics.startSystem(Phaser.Physics.ARCADE);
- 
-    //Add a platforms group to hold all of our tiles, and create a bunch of them
-    me.platforms = me.game.add.group();
-    me.platforms.enableBody = true;
-    me.platforms.createMultiple(250, 'tile');
-	},
+  preload: function() {
+    this.load.image( 'hero', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/dude.png' );
+    this.load.image( 'pixel', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/pixel_1.png' );
+  },
 
-	update: function() {
- 
-    var me = this;
- 
-    //Make the sprite collide with the ground layer
-    me.game.physics.arcade.collide(me.player, me.platforms);
- 
- 
-    //Check if the player is touching the bottom
-    if(me.player.body.position.y >= me.game.world.height - me.player.body.height){
-        me.gameOver();
+  create: function() {
+    // background color
+    this.stage.backgroundColor = '#6bf';
+
+    // scaling
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.maxWidth = this.game.width;
+    this.scale.maxHeight = this.game.height;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.scale.setScreenSize( true );
+
+    // physics
+    this.physics.startSystem( Phaser.Physics.ARCADE );
+
+    // camera and platform tracking vars
+    this.cameraYMin = 99999;
+    this.platformYMin = 99999;
+
+    // create platforms
+    this.platformsCreate();
+
+    // create hero
+    this.heroCreate();
+
+    // cursor controls
+    this.cursor = this.input.keyboard.createCursorKeys();
+  },
+
+  update: function() {
+    // this is where the main magic happens
+    // the y offset and the height of the world are adjusted
+    // to match the highest point the hero has reached
+    this.world.setBounds( 0, -this.hero.yChange, this.world.width, this.game.height + this.hero.yChange );
+
+    // the built in camera follow methods won't work for our needs
+    // this is a custom follow style that will not ever move down, it only moves up
+    this.cameraYMin = Math.min( this.cameraYMin, this.hero.y - this.game.height + 130 );
+    this.camera.y = this.cameraYMin;
+
+    // hero collisions and movement
+    this.physics.arcade.collide( this.hero, this.platforms );
+    this.heroMove();
+
+    // for each plat form, find out which is the highest
+    // if one goes below the camera view, then create a new one at a distance from the highest one
+    // these are pooled so they are very performant
+    this.platforms.forEachAlive( function( elem ) {
+      this.platformYMin = Math.min( this.platformYMin, elem.y );
+      if( elem.y > this.camera.y + this.game.height ) {
+        elem.kill();
+        this.platformsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.platformYMin - 100, 50 );
+      }
+    }, this );
+  },
+
+  shutdown: function() {
+    // reset everything, or the world will be messed up
+    this.world.setBounds( 0, 0, this.game.width, this.game.height );
+    this.cursor = null;
+    this.hero.destroy();
+    this.hero = null;
+    this.platforms.destroy();
+    this.platforms = null;
+  },
+
+  platformsCreate: function() {
+    // platform basic setup
+    this.platforms = this.add.group();
+    this.platforms.enableBody = true;
+    this.platforms.createMultiple( 10, 'pixel' );
+
+    // create the base platform, with buffer on either side so that the hero doesn't fall through
+    this.platformsCreateOne( -16, this.world.height - 16, this.world.width + 16 );
+    // create a batch of platforms that start to move up the level
+    for( var i = 0; i < 9; i++ ) {
+      this.platformsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.world.height - 100 - 100 * i, 50 );
     }
- //Make the sprite jump when the up key is pushed
-if(me.cursors.up.isDown && me.player.body.wasTouching.down) {
-    me.player.body.velocity.y = -1400;
-}
-//Make the player go left
-if(me.cursors.left.isDown){
-    me.player.body.velocity.x += -30;
-}
-//Make the player go right
-if(me.cursors.right.isDown){
-    me.player.body.velocity.x += 30;
-}
-},
+  },
 
-	gameOver: function(){
-		this.game.state.start('GameOver');
-	}
-	
-	//Add the player to the screen
-me.createPlayer();
-//The spacing for the initial platforms
-me.spacing = 300;
- 
-//Create the inital on screen platforms
-me.initPlatforms();
-me.timer = game.time.events.loop(2000, me.addPlatform, me);
-//Enable cursor keys so we can create some controls
-me.cursors = me.game.input.keyboard.createCursorKeys(); 
-	//Create the score variable
-me.score = 0;
- 
-//Create the score label
-me.createScore();
-};
-addTile: function(x, y){
- 
-    var me = this;
- 
-    //Get a tile that is not currently on screen
-    var tile = me.platforms.getFirstDead();
- 
-    //Reset it to the specified coordinates
-    tile.reset(x, y);
-    tile.body.velocity.y = 150;
-    tile.body.immovable = true;
- 
-    //When the tile leaves the screen, kill it
-    tile.checkWorldBounds = true;
-    tile.outOfBoundsKill = true;   
-},
- 
-addPlatform: function(y){
- 
-    var me = this;
- 
-    //If no y position is supplied, render it just outside of the screen
-if(typeof(y) == "undefined"){
-    y = -me.tileHeight;
-    //Increase the players score
-    me.incrementScore();
-}
- 
-    //Work out how many tiles we need to fit across the whole screen
-    var tilesNeeded = Math.ceil(me.game.world.width / me.tileWidth);
- 
-    //Add a hole randomly somewhere
-    var hole = Math.floor(Math.random() * (tilesNeeded - 3)) + 1;
- 
-    //Keep creating tiles next to each other until we have an entire row
-    //Don't add tiles where the random hole is
-    for (var i = 0; i < tilesNeeded; i++){
-        if (i != hole && i != hole + 1){
-            this.addTile(i * me.tileWidth, y);
-        }          
+  platformsCreateOne: function( x, y, width ) {
+    // this is a helper function since writing all of this out can get verbose elsewhere
+    var platform = this.platforms.getFirstDead();
+    platform.reset( x, y );
+    platform.scale.x = width;
+    platform.scale.y = 16;
+    platform.body.immovable = true;
+    return platform;
+  },
+
+  heroCreate: function() {
+    // basic hero setup
+    this.hero = game.add.sprite( this.world.centerX, this.world.height - 36, 'hero' );
+    this.hero.anchor.set( 0.5 );
+    
+    // track where the hero started and how much the distance has changed from that point
+    this.hero.yOrig = this.hero.y;
+    this.hero.yChange = 0;
+
+    // hero collision setup
+    // disable all collisions except for down
+    this.physics.arcade.enable( this.hero );
+    this.hero.body.gravity.y = 500;
+    this.hero.body.checkCollision.up = false;
+    this.hero.body.checkCollision.left = false;
+    this.hero.body.checkCollision.right = false;
+  },
+
+  heroMove: function() {
+    // handle the left and right movement of the hero
+    if( this.cursor.left.isDown ) {
+      this.hero.body.velocity.x = -200;
+    } else if( this.cursor.right.isDown ) {
+      this.hero.body.velocity.x = 200;
+    } else {
+      this.hero.body.velocity.x = 0;
     }
-}
-me.timer = game.time.events.loop(2000, me.addPlatform, me);
-initPlatforms: function(){
- 
-    var me = this,
-        bottom = me.game.world.height - me.tileHeight,
-        top = me.tileHeight;
- 
-    //Keep creating platforms until they reach (near) the top of the screen
-    for(var y = bottom; y > top - me.tileHeight; y = y - me.spacing){
-        me.addPlatform(y);
+
+    // handle hero jumping
+    if( this.cursor.up.isDown && this.hero.body.touching.down ) {
+      this.hero.body.velocity.y = -350;
+    } 
+    
+    // wrap world coordinated so that you can warp from left to right and right to left
+    this.world.wrap( this.hero, this.hero.width / 2, false );
+
+    // track the maximum amount that the hero has travelled
+    this.hero.yChange = Math.max( this.hero.yChange, Math.abs( this.hero.y - this.hero.yOrig ) );
+    
+    // if the hero falls below the camera view, gameover
+    if( this.hero.y > this.cameraYMin + this.game.height && this.hero.alive ) {
+      this.state.start( 'Play' );
     }
+  }
 }
-//The spacing for the initial platforms
-me.spacing = 300;
- 
-//Create the inital on screen platforms
-me.initPlatforms();
-createPlayer: function(){
- 
-    var me = this;
- 
-    //Add the player to the game by creating a new sprite
-    me.player = me.game.add.sprite(me.game.world.centerX, me.game.world.height - (me.spacing * 2 + (3 * me.tileHeight)), 'player');
- 
-    //Set the players anchor point to be in the middle horizontally
-    me.player.anchor.setTo(0.5, 1.0);
- 
-    //Enable physics on the player
-    me.game.physics.arcade.enable(me.player);
- 
-    //Make the player fall by applying gravity
-    me.player.body.gravity.y = 2000;
- 
-    //Make the player collide with the game boundaries
-    me.player.body.collideWorldBounds = true;
- 
-    //Make the player bounce a little
-    me.player.body.bounce.y = 0.1;
- 
-},
-gameOver: function(){
-    this.game.state.start('Main');
-},
-	createScore: function(){
- 
-    var me = this;
- 
-    var scoreFont = "100px Arial";
- 
-    me.scoreLabel = me.game.add.text((me.game.world.centerX), 100, "0", {font: scoreFont, fill: "#fff"});
-    me.scoreLabel.anchor.setTo(0.5, 0.5);
-    me.scoreLabel.align = 'center';
- 
-},
- 
-incrementScore: function(){
- 
-    var me = this;
- 
-    me.score += 1;  
-    me.scoreLabel.text = me.score;     
- 
-},
+
+var game = new Phaser.Game( 300, 500, Phaser.CANVAS, '' );
+game.state.add( 'Play', Jumper.Play );
+game.state.start( 'Play' );
